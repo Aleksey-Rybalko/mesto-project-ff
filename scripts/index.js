cacf5d6663978f361f58dd6ffc9e63657d111513
Aleksey Rybalko <ip-alexejrybalko@yandex.ru>
2025-05-31 12:42:23 +0300

    feat(cards): add modular card creation with delete handler

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplare = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+
+const cardDelete = function (element) {
+  element.remove();
+}
+
+const cardElementAdd = function (card, cardDel) {
+  
+  // клон темплейта
+  const cardElement = cardTemplare
+    .querySelector(".places__item")
+    .cloneNode(true);
+
+  // содержимое
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__title").textContent = card.name;
+
+  // вывод
+  placesList.append(cardElement);
+
+  // удаление
+  const btnDel = cardElement.querySelector(".card__delete-button");
+  btnDel.addEventListener("click", () => cardDel(cardElement));
+};
+
+// вызов
+initialCards.forEach(item => cardElementAdd(item, cardDelete))
+
